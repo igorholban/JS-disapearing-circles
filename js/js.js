a233796cdf5cdddc61fff5b7d61e04d6f0fcf4be
igor <holban.igor@gmail.com>
2017-11-10 13:55:06 +0200

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,86 @@
+function displayRed() {
+    var x = document.getElementById("red");
+    x.className = "afterplay";
+    var dolong = document.getElementById("dolong");
+    dolong.play();
+
+}
+
+function displayBlue() {
+    var x = document.getElementById("blue");
+    x.className = "afterplay";
+    var relong = document.getElementById("relong");
+    relong.play();
+}
+
+function displayYellow() {
+    var x = document.getElementById("yellow");
+    x.className = "afterplay";
+    var milong = document.getElementById("milong");
+    milong.play();
+}
+
+function displayGreen() {
+    var x = document.getElementById("green");
+    x.className = "afterplay";
+    var falong = document.getElementById("falong");
+    falong.play();
+}
+
+function displayOrange() {
+    var x = document.getElementById("orange");
+    x.className = "afterplay";
+    var sollong = document.getElementById("sollong");
+    sollong.play();
+}
+
+function displayAquamarine() {
+    var x = document.getElementById("aquamarine");
+    x.className = "afterplay";
+    var falong = document.getElementById("falong");
+    falong.play();
+}
+
+function displayOlive() {
+    var x = document.getElementById("olive");
+    x.className = "afterplay";
+    var silong = document.getElementById("silong");
+    silong.play();
+}
+
+function backolive() {
+    var x = document.getElementById("olive");
+    x.className = "defaultolive";
+}
+
+function backaquamarine() {
+    var x = document.getElementById("aquamarine");
+    x.className = "defaultaquamarine";
+}
+
+
+function backorange() {
+    var x = document.getElementById("orange");
+    x.className = "defaultorange";
+}
+
+
+function backred() {
+    var x = document.getElementById("red");
+    x.className = "defaultred";
+}
+
+function backblue() {
+    var x = document.getElementById("blue");
+    x.className = "defaultblue";
+}
+
+function backyellow() {
+    var x = document.getElementById("yellow");
+    x.className = "defaultyellow";
+}
+
+function backgreen() {
+    var x = document.getElementById("green");
+    x.className = "defaultgreen";
+}
